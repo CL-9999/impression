aa9ebf8845d0c2b22bcccae1b3a3fc9eb1271744
Bear <1942496795@qq.com>
2021-07-20 14:20:04 +0800

    Controller update

---
+++ src/main/java/com/itbear/impression/controller/admin/CategoryController.java
@@ -0,0 +1,9 @@
+package com.itbear.impression.controller.admin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/category")
+public class CategoryController {
+
+
+}
